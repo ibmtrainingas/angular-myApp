132e291774169cef6783d1e8c9131c5d621eade8
ibmabhishek <erren98@gmail.com>
2021-04-15 11:56:40 +0530

    Added 2 way binding

--- src/app/userform/userform.component.ts
+++ src/app/userform/userform.component.ts
@@ -8,8 +8,14 @@ import { Component, OnInit } from '@angular/core';
 export class UserformComponent implements OnInit {
   title: String = 'Userform';
   firstname: String = 'Ram';
+  age: number = 24;
   constructor() { }
-
+  save() {
+    console.log('working ...');
+    console.log(this.firstname);
+    //this.firstname = 'John';
+    console.log(this.age);
+  }
   ngOnInit(): void {
   }
 
