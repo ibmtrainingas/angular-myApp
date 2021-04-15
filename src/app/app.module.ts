203e91001d30316a2f6ef26b78dcff9ca799d63e
ibmabhishek <erren98@gmail.com>
2021-04-15 11:26:17 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { UserformComponent } from './userform/userform.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    UserformComponent
+  ],
+  imports: [
+    BrowserModule, FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
